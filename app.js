500bdfa671ab1937e6692fe6a0cc380a70afcfe2
joe-nomad <1921731@iub.edu.bd>
2022-07-03 12:10:58 +0600

    portfolio 1

--- app.js
+++ app.js
@@ -6,4 +6,27 @@ letter = document.querySelectorAll(".text-zone span");
 					this.classList.remove("bounce");
 				}.bind(this), 1000)
 			})
-		})
+		});
+
+
+const toggleBtn = document.querySelector(".sidebar-toggle")
+const menu = document.querySelector(".main-menu");
+const closeBtn = document.querySelector(".close-btn");
+const ab = document.getElementById("about");
+console.log(ab);
+const skill = document.getElementById("skill");
+const education = document.getElementById("education");
+const contact = document.getElementById("contact");
+// const work = document.getElementById("about");
+
+toggleBtn.addEventListener("click", function () {
+	menu.classList.add("open-nav");
+	
+});
+closeBtn.addEventListener("click", function () {
+	menu.classList.remove("open-nav");
+});
+ab.addEventListener("click", function () {
+	console.log("hi");
+});
+
